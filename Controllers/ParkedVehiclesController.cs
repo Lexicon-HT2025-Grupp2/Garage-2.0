288a4b02cdc3f3c2638b392245d4437d190c308d
Felix Havlind <havlindfelix@gmail.com>
2025-12-17 15:09:49 +0100

    Initial implementation of _FlashMessages.cshtml for feedback for ParkedVehiclesController.cs methods.

--- Controllers/ParkedVehiclesController.cs
+++ Controllers/ParkedVehiclesController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Garage_2._0.Data;
 using Garage_2._0.Models;
@@ -63,8 +58,10 @@ namespace Garage_2._0.Controllers
             {
                 _context.Add(parkedVehicle);
                 await _context.SaveChangesAsync();
+                TempData["SuccessMessage"] = "Vehicle successfully entered the garage";
                 return RedirectToAction(nameof(Index));
             }
+            
             return View(parkedVehicle);
         }
 
