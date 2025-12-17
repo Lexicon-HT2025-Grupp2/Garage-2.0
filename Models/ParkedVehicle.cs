0706412fcad2d946f5f37ae80750b71c3f1d830c
Tony Aldervall <tony.aldervall@iths.se>
2025-12-17 09:06:31 +0100

    added vehicle class

---
+++ Models/ParkedVehicle.cs
@@ -0,0 +1,14 @@
+﻿namespace Garage_2._0.Models
+{
+    public class ParkedVehicle
+    {
+        public string RegistrationNumber { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NumberOfWheels { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public string Note { get; set; }
+    }
+}
