0bffb6ad5848836fbfcc4715d0d6c77f21cc490c
Tony Aldervall <tony.aldervall@iths.se>
2025-12-17 09:44:36 +0100

    added viewmodels for vehicle

---
+++ Models/DetailedParkedVehicleViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Garage_2._0.Models
+{
+    public class DetailedParkedVehicleViewModel
+    {
+        public string RegistrationNumber { get; set; }
+        public string Type { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public TimeSpan ParkedDuration => DateTime.Now - ArrivalTime;
+    }
+}
