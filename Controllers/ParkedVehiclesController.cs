a62b8aa2afda5bcf918214c2af3a79b164c4f4e0
Felix Havlind <havlindfelix@gmail.com>
2025-12-18 09:44:09 +0100

    Added error flash message and fixed ParkedVehiclesController.cs flash messages. Minor cleanup

--- Controllers/ParkedVehiclesController.cs
+++ Controllers/ParkedVehiclesController.cs
@@ -3,8 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Garage_2._0.Data;
 using Garage_2._0.Models;
 using Garage_2._0.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Garage_2._0.Controllers
 {
@@ -32,7 +30,7 @@ namespace Garage_2._0.Controllers
         }
 
         // GET: ParkedVehicles/Details/5
-        public async Task<IActionResult> Details(string id)
+        public async Task<IActionResult> Details(string? id)
         {
             if (id == null)
             {
@@ -72,11 +70,12 @@ namespace Garage_2._0.Controllers
             if (!ModelState.IsValid)
                 return View(parkedVehicle);
 
-            bool exists = await _context.ParkedVehicle
+            var exists = await _context.ParkedVehicle
                 .AnyAsync(v => v.RegistrationNumber == parkedVehicle.RegistrationNumber);
 
             if (exists)
             {
+                TempData["ErrorMessage"] = "Failed to enter vehicle into garage.";
                 ModelState.AddModelError(nameof(ParkedVehicle.RegistrationNumber),
                     "Car already exists in the garage.");
 
@@ -85,12 +84,12 @@ namespace Garage_2._0.Controllers
 
             _context.Add(parkedVehicle);
             await _context.SaveChangesAsync();
-
+            TempData["SuccessMessage"] = "Vehicle entered garage successfully.";
             return RedirectToAction(nameof(Index));
         }
 
         // GET: ParkedVehicles/Edit/5
-        public async Task<IActionResult> Edit(string id)
+        public async Task<IActionResult> Edit(string? id)
         {
             if (id == null)
             {
@@ -143,7 +142,7 @@ namespace Garage_2._0.Controllers
         }
 
         // GET: ParkedVehicles/CheckOut/5
-        public async Task<IActionResult> CheckOut(string id)
+        public async Task<IActionResult> CheckOut(string? id)
         {
             if (id == null)
             {
