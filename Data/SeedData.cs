46a090431c9f5e5f8c1a6b32882c151ed3529fab
Felix Havlind <havlindfelix@gmail.com>
2026-01-15 12:05:34 +0100

    Added default users for both roles

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -1,10 +1,24 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Garage_2._0.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Garage_2._0.Data;
 
 public static class SeedData
 {
     private static readonly string[] roles = [ "Admin", "Member" ];
+    private static readonly ApplicationUser adminUser = new ApplicationUser
+    {
+        UserName = "admin@email.com",
+        Email = "admin@email.com",
+        EmailConfirmed = true,
+    };
+    private static readonly ApplicationUser memberUser = new ApplicationUser
+    {
+        UserName = "member@email.com",
+        Email = "member@email.com",
+        EmailConfirmed = true,
+    };
+    private static readonly string password = "Passw0rd!";
 
     public static async Task InitializeAsync(IServiceProvider serviceProvider)
     {
@@ -22,6 +36,32 @@ public static class SeedData
             if (!result.Succeeded) {
                 throw new Exception("Failed to seed roles");
             }
+        }
+
+        var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+        if (await userManager.FindByEmailAsync(adminUser.Email!) == null)
+        {
+            var result = await userManager.CreateAsync(adminUser, password);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception($"Failed to seed user: {adminUser}");
+            }
+
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+        }
+
+        if (await userManager.FindByEmailAsync(memberUser.Email!) == null)
+        {
+            var result = await userManager.CreateAsync(memberUser, password);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception($"Failed to seed user: {memberUser}");
+            }
+
+            await userManager.AddToRoleAsync(memberUser, "Member");
         }
     }
 }
