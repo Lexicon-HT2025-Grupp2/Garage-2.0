d870868b9f534595014fe83bd3103eba69b53c3b
Felix Havlind <havlindfelix@gmail.com>
2026-01-15 11:48:03 +0100

    Added role seeding functionality

---
+++ Data/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Garage_2._0.Data;
+
+public static class SeedData
+{
+    private static readonly string[] roles = [ "Admin", "Member" ];
+
+    public static async Task InitializeAsync(IServiceProvider serviceProvider)
+    {
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    
+        foreach (var role in roles)
+        {
+            if (await roleManager.RoleExistsAsync(role))
+            {
+                continue;
+            }
+
+            var result = await roleManager.CreateAsync(new IdentityRole(role));
+
+            if (!result.Succeeded) {
+                throw new Exception("Failed to seed roles");
+            }
+        }
+    }
+}
